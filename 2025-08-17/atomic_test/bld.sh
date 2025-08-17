30a0e915738a8b60b5b935df791aa1eca89d8479
oldzhu <wp_xdzhu@hotmail.com>
2025-08-17 15:58:23 +0800

    add one build command into bld.sh so both include logging and real compilation could done in one command

--- 2025-08-17/atomic_test/bld.sh
+++ 2025-08-17/atomic_test/bld.sh
@@ -1,3 +1,5 @@
 /home/oldzhu/buildroot/output/build/host-lldb-origin_main/llvm/buildroot-build/bin/clang --target=aarch64-buildroot-linux-gnu --gcc-toolchain=/home/oldzhu/buildroot/output/host/aarch64-buildroot-linux-gnu/sysroot/usr --sysroot=/home/oldzhu/buildroot/output/host/aarch64-buildroot-linux-gnu/sysroot -v -E -x c atomic_test.c
 
 /home/oldzhu/buildroot/output/build/host-lldb-origin_main/llvm/buildroot-build/bin/clang --target=aarch64-buildroot-linux-gnu --gcc-toolchain=/home/oldzhu/buildroot/output/host/aarch64-buildroot-linux-gnu/sysroot/usr --sysroot=/home/oldzhu/buildroot/output/host/aarch64-buildroot-linux-gnu/sysroot -c atomic_test.c
+
+/home/oldzhu/buildroot/output/build/host-lldb-origin_main/llvm/buildroot-build/bin/clang --target=aarch64-buildroot-linux-gnu --gcc-toolchain=/home/oldzhu/buildroot/output/host/aarch64-buildroot-linux-gnu/sysroot/usr --sysroot=/home/oldzhu/buildroot/output/host/aarch64-buildroot-linux-gnu/sysroot -H -v -c atomic_test.c
